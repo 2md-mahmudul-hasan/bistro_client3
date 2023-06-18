d231f8ab4a88ec6df25c8718a60809025f8d3a9b
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-06-18 06:29:12 +0600

    login and signUp

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,17 +1,52 @@
-import React, { createContext, useState } from 'react';
 
-import { getAuth } from "firebase/auth";
+
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/Firebase';
+import { createContext } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 export const authContext = createContext(null)
 
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
+  //createUser
+  const createUser = (email, password) => {
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+  //signinuser
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+  //logout
+  const logOut = () => {
+    setLoading(true)
+    return signOut(auth).then().catch(err => {
+      console.log(err)
+    })
+  }
+  //manage users
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user)
+        setLoading(false)
+      }
+    });
+    return () => {
+      unsubscribe()
+    }
+  })
+
   const auth = getAuth(app)
   const authInfo = {
     user,
     loading,
-    getAuth
+    getAuth,
+    createUser,
+    login,
+    logOut
   }
   return (
     <authContext.Provider value={authInfo}>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,39 +1,24 @@
+
 import { useContext } from 'react';
 import { useEffect, useRef, useState } from 'react';
-import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+
 import { authContext } from '../../Providers/AuthProviders';
 import { Link } from 'react-router-dom';
 
 
 
-const Login = () => {
-  const captchaRef = useRef(null)
-  const [disabled, setDisabled] = useState(true)
+const SignUp = () => {
 
 
-  const { login } = useContext(authContext)
 
-  useEffect(() => {
-    loadCaptchaEnginge(6);
-  }, [])
-
-  const validateCaptchaText = () => {
-    const captchaText = captchaRef.current.value;
-    if (validateCaptcha(captchaText) == true) {
-      setDisabled(false)
-    }
-
-    else {
-      alert('Captcha Does Not Match');
-    }
-  }
-  const handleLoginForm = (e) => {
+  const { createUser } = useContext(authContext)
+  const handleSignUpForm = (e) => {
     e.preventDefault()
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password)
-    login(email, password)
+    createUser(email, password)
       .then(result => {
         const user = result.user;
         console.log(user)
@@ -45,11 +30,11 @@ const Login = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col ">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
+          <h1 className="text-5xl font-bold">Sign up now!</h1>
 
         </div>
         <div className="card w-[500px] shadow-2xl bg-base-100">
-          <form onSubmit={handleLoginForm} className="card-body">
+          <form onSubmit={handleSignUpForm} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -64,24 +49,16 @@ const Login = () => {
               <input name="password" type="text" placeholder="password" className="input input-bordered" />
             </div>
 
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text"> <LoadCanvasTemplate /></span>
-              </label>
-              <input ref={captchaRef} name="captcha" type="text" placeholder="type above text" className="input input-bordered" />
-              <button onClick={validateCaptchaText} className='btn btn-secondary btn-xs my-4'>Validate captcha</button>
-            </div>
+
             <div className="form-control mt-6">
-              <input disabled={disabled} type="submit" className="btn btn-primary" value='login' />
+              <input type="submit" className="btn btn-primary" value='login' />
             </div>
           </form>
-          <div>
-            not regeistered <Link to='/signUp'>sign up here</Link>
-          </div>
+          <div>Already sign up <Link to='/login'> log in here</Link></div>
         </div>
       </div>
     </div>
   )
 };
 
-export default Login;
+export default SignUp;
